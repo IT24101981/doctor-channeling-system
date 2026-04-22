14ddfbbfe024ef34e1d243560219bd24628601a0
IT24101236 <janidukasuntha123@gmail.com>
2026-04-22 17:36:40 +0530

    default mistake fix

--- backend/add_marked_column.js
+++ backend/add_marked_column.js
@@ -2,7 +2,7 @@ const db = require('./config/db');
 
 async function addMarkedColumn() {
     try {
-        await db.execute('ALTER TABLE doc_availability_slots ADD COLUMN Marked TINYINT(1) DEFAULT 1');
+        await db.execute('ALTER TABLE doc_availability_slots ADD COLUMN Marked TINYINT(1) DEFAULT 0');
         console.log('Successfully added Marked column to doc_availability_slots');
     } catch (error) {
         if (error.code === 'ER_DUP_FIELDNAME') {
